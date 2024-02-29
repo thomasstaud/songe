3c21fe5fb95c815ae4d091c465024d2b10543b90
Thomas Staud <thstaud@tsn.at>
2024-02-29 07:50:51 +0100

    replaced ip with localhost

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite'
+import dns from 'dns'
 import vue from '@vitejs/plugin-vue'
+
+dns.setDefaultResultOrder('verbatim')
 
 // https://vitejs.dev/config/
 export default defineConfig({
